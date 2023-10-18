4b21184cc766d30a1ad6e3baae563af1e5299c31
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2023-10-18 06:04:00 +0300

    removed the wacky animations from mobile

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -21,7 +21,7 @@ export default function SearchBar() {
     router.push("/search/" + searchQuery);
   };
   return (
-    <div className="w-96 max-w-md ">
+    <div className="w-96 max-w-full ">
       <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden shadow-lg">
         <div className="grid place-items-center h-full w-12 text-gray-300">
           <svg
