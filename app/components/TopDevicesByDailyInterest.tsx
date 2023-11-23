8b0d690a6d3614a66aa17afcd5dcf77c0b4554bb
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2023-11-23 16:44:24 +0300

    changed some mobile margin

--- app/components/TopDevicesByDailyInterest.tsx
+++ app/components/TopDevicesByDailyInterest.tsx
@@ -18,7 +18,7 @@ export default async function TopDevicesByFans(topDevices: topDevices) {
       <h2 className="text-5xl text-[#005bea] text-center mb-6 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] mt-full max-md:ml-auto max-md:text-center max-md:mt-32">
         {topDevices[0].category}
       </h2>
-      <div className="flex flex-wrap flex-row justify-evenly">
+      <div className="flex flex-wrap flex-row justify-evenly ">
         {topDevices[0].list.map((device, index: number) => {
           let isEven = index % 2 === 0;
           const deviceDetail = deviceDetails[index];
