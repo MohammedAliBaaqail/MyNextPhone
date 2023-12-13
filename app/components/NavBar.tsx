9bf38d2161e202d40d827941ee39a2a0796aa77c
Mohammed Ali <mohammed.ali.baaqail@gmail.com>
2023-12-13 20:38:19 +0300

    changed link to a tag

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,14 +9,14 @@ export default function navBar() {
   return (
 <nav
   className=" h-[10vh] nav_bg top-0 px-6   w-full flex flex-col justify-between md:flex-row items-center   py-2 text-neutral-500    lg:py-4 max-md:h-[15vh]">
-    <Link className="text-xl text-neutral-800 " href="/">
+    <a className="text-xl text-neutral-800 " href="/">
       <img
         src="/images/logo.png"
         alt="logo"
         width={350}
         height={350}
       />
-        </Link>
+        </a>
         <SearchBar/>
         {/* <Link
             className="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 text-xl font-medium "
